939a37a7630f2e191c52f14dc1750e0645a3bac8
Ashish <elx.ashish@gmail.com>
2025-01-27 18:46:26 +0530

    weekly task

---
+++ src/ex01/Lab007_Comments_ShortCuts.java
@@ -0,0 +1,11 @@
+package ex01;
+
+public class Lab007_Comments_ShortCuts {
+    public static void main(String[] args) {
+        // This code will print the Hi
+        // Jr QA - this will print the hi in console
+
+
+        System.out.println("Hi");
+    }
+}
