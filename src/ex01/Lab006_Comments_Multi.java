bd929720c655ebe93a660897219ec2930b83ad47
Ashish <elx.ashish@gmail.com>
2025-01-27 18:44:14 +0530

    weekly task

---
+++ src/ex01/Lab006_Comments_Multi.java
@@ -0,0 +1,12 @@
+package ex01;
+
+public class Lab006_Comments_Multi {
+    public static void main(String[] args) {
+        /**
+         * Author : Pramod Dutta
+         * Course :  ATB10x
+         *
+         */
+        System.out.println("Multi Comment");
+    }
+}
