ded8b054f694e65512a40a8624ee80cd17513b50
Ashish <elx.ashish@gmail.com>
2025-01-27 18:42:35 +0530

    weekly task

---
+++ src/ex01/Lab005_Comments.java
@@ -0,0 +1,9 @@
+package ex01;
+
+public class Lab005_Comments {
+
+    public static void main(String[] args) {
+        // This is a Comment, It will be ignored by JVM/ Compiler
+        //System.out.println("Hi");
+    }
+}
