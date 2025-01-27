550e548316c898e32934f52fe0a352d4aa50f513
Ashish <elx.ashish@gmail.com>
2025-01-27 18:48:34 +0530

    weekly task

---
+++ src/ex01/Lab008_Comment.java
@@ -0,0 +1,16 @@
+package ex01;
+
+public class Lab008_Comment {
+
+
+    /**
+     * Author - Pramod
+     * Course :  ATB10x
+     * This is a documentation comment
+     * @param args command-line arguments
+     * @return void
+     */
+    public static void main(String[] args) {
+        // Method implementation
+    }
+}
