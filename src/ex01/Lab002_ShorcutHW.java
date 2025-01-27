e1da2a4348421137182a4f11e54d477e3d488fbf
Ashish <elx.ashish@gmail.com>
2025-01-27 18:35:26 +0530

    weekly task

---
+++ src/ex01/Lab002_ShorcutHW.java
@@ -0,0 +1,11 @@
+package ex01;
+
+public class Lab002_ShorcutHW {
+    public static void main(String[] args) {
+        System.out.println("Amol");
+    }
+
+    public static void main() {
+        System.out.println("Amol");
+    }
+}
