510befe50008b058293cfc89aa091c2fe2ddb540
Ashish <elx.ashish@gmail.com>
2025-01-28 13:58:54 +0530

    weekly task

---
+++ src/ex02_Java_Basic/Lab021_Boolean.java
@@ -0,0 +1,14 @@
+package ex02_Java_Basic;
+
+public class Lab021_Boolean {
+    public static void main(String[] args) {
+
+        boolean is_Ashish_married = true;
+        boolean is_Ashish_married1 = false;
+        boolean Ashish_has_audi = false;
+        boolean this_is_a_long_name_that_student_asked_from_us_that_is_it_possible = true;
+
+
+
+    }
+}
