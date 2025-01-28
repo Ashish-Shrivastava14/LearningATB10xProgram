bf45b12d6c94344bbe24e8815f1eb1ad170db7ce
Ashish <elx.ashish@gmail.com>
2025-01-28 13:30:54 +0530

    weekly task

---
+++ src/ex02_Java_Basic/Lab014_Variables.java
@@ -0,0 +1,12 @@
+package ex02_Java_Basic;
+
+public class Lab014_Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+    public void main(String args) {
+
+    }
+}
