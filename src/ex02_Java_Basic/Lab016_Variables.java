13403747d802a51de5534f74cc46db68f864f9f2
Ashish <elx.ashish@gmail.com>
2025-01-28 13:36:31 +0530

    weekly task

---
+++ src/ex02_Java_Basic/Lab016_Variables.java
@@ -0,0 +1,14 @@
+package ex02_Java_Basic;
+
+public class Lab016_Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        // data type variable_name =  variable_value
+
+        int age = 65;
+        age = 65 + 1;
+        System.out.println(age);
+
+
+    }
+}
