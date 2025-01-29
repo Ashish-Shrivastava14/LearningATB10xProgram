14f5cb97a1b7da827600b8cccf763f2e089a0f11
Ashish <elx.ashish@gmail.com>
2025-01-29 09:11:06 +0530

    weekly task

---
+++ src/ex02_Java_Basic/Lab027_Multiple_Variables.java
@@ -0,0 +1,11 @@
+package ex02_Java_Basic;
+
+public class Lab027_Multiple_Variables {
+    public static void main(String[] args) {
+        int a = 10, b = 23, c = 45;
+
+        int d = 10;// int d = 23;
+
+
+    }
+}
---
+++ src/ex02_Java_Basic/Lab028_Multiple_Class.java
@@ -0,0 +1,18 @@
+package ex02_Java_Basic;
+
+public class Lab028_Multiple_Class {
+    public static void main(String[] args) {
+
+    }
+}
+
+class A {
+
+}
+
+class B {
+}
+
+class C {
+}
+
