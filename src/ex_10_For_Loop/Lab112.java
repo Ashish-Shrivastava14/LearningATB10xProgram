c0596d23bbea54c34e8e3c0abc2a6602568603aa
Ashish <elx.ashish@gmail.com>
2025-02-01 08:31:59 +0530

    Weekly task

---
+++ src/ex_10_For_Loop/Lab112.java
@@ -0,0 +1,37 @@
+package ex_10_For_Loop;
+
+public class Lab112 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("Ashish");
+        }
+        // Exp and Result Table
+        // | i=0 , 1 | System.out.println("Ashish");
+        // | i=1 , 2 | System.out.println("Ashish");
+        // | i=2 , 3 | System.out.println("Ashish");
+        // | i=3 , 4 | System.out.println("Ashish");
+        // | i=4 , 5 | System.out.println("Ashish");
+        // | i=5 , 6 | System.out.println("Ashish");
+        // | i=6 , 7 | System.out.println("Ashish");
+        // | i=7 , 8 | System.out.println("Ashish");
+        // | i=8 , 9 | System.out.println("Ashish");
+        // | i=9 , 10 | System.out.println("Ashish");
+        // | i=10 , False | NA
+
+
+        for (int i = 1; i <= 10 ; i++) { // 1 to 10
+            System.out.println("Shrivastava");
+        }
+        // | i=1 , 1 |
+        // | i=2 , 2 |
+        // | i=3 , 3 |
+        // | i=4 , 4 |
+        // | i=5 , 5 |
+        // | i=6 , 6 |
+        // | i=7 , 7 |
+        // | i=8 , 8 |
+        // | i=9 , 9 |
+        // | i=10 , 10 |
+
+    }
+}
