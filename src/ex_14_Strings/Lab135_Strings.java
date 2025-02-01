8d3ed1ab874c1d5321698c90361b917ba7c510b7
Ashish <elx.ashish@gmail.com>
2025-02-01 08:37:04 +0530

    Weekly task

---
+++ src/ex_14_Strings/Lab135_Strings.java
@@ -0,0 +1,16 @@
+package ex_14_Strings;
+
+public class Lab135_Strings {
+    public static void main(String[] args) {
+        char c  = 'P';
+        System.out.println(c);
+
+        String s1 = "Ashish";
+        System.out.println(s1.length());
+        System.out.println(s1.toLowerCase());
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.concat("Shrivastava"));
+
+
+    }
+}
---
+++ src/ex_14_Strings/Lab136_Interview.java
@@ -0,0 +1,9 @@
+package ex_14_Strings;
+
+public class Lab136_Interview {
+    public static void main(String[] args) {
+        String name = "ashish";
+        name = name.toUpperCase();
+        System.out.println(name);
+    }
+}
