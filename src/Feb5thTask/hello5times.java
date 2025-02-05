3859ac162291b12f9477905f9a28751837c8a748
Ashish <elx.ashish@gmail.com>
2025-02-05 16:14:26 +0530

    Weekly task

---
+++ src/Feb5thTask/hello5times.java
@@ -0,0 +1,9 @@
+package Feb5thTask;
+
+// Print "Hello" 5 times using for
+public class hello5times {
+    public static void main(String[] args) {
+        String Str="Hello";
+        System.out.print(Str.repeat(5));
+    }
+}
